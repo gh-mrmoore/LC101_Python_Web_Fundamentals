bf1d3359a7b859b9c121510c805531d2c390c2e3
Matthew Moore <matt.rmoore@gmail.com>
2018-10-29 18:45:49 -0500

    html updates and a couple of python edits

--- main.py
+++ main.py
@@ -16,16 +16,28 @@ class Task(db.Model):
     def __init__(self, name):
         self.name = name
 
-tasks = []
-
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
         task = request.form['task']
-        tasks.append(task)
+        new_task = Task(task_name)
+        db.session.add(new_task)
+        db.session.commit()
+    
+    tasks = Task.query.all()
 
     return render_template('todos.html', title="To Do List", tasks=tasks)
+
+@app.route('/delete-task', methods=['POST'])
+def delete_task():
+
+    task_id = int(request.form['task-id'])
+    task = Task.query.get(task_id)
+    db.session.delete(task)
+    db.session.commit()
+
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
