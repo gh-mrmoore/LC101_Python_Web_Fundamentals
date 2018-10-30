ac069a76776f0576bf67a28b78670ce185ae46dc
Matthew Moore <matt.rmoore@gmail.com>
2018-10-29 23:27:59 -0500

    Working copy with new and completed tasks sections

--- main.py
+++ main.py
@@ -12,29 +12,33 @@ class Task(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)   #every persistent class will need to have an id for database insertion
     name = db.Column(db.String(120))   #creating column 'name' with 120 characters maximum
+    completed = db.Column(db.Boolean)
 
     def __init__(self, name):
         self.name = name
+        self.completed = False
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
-        task = request.form['task']
+        task_name = request.form['task']
         new_task = Task(task_name)
         db.session.add(new_task)
         db.session.commit()
     
-    tasks = Task.query.all()
+    tasks = Task.query.filter_by(completed=False).all()
+    completed_tasks = Task.query.filter_by(completed=True).all()
 
-    return render_template('todos.html', title="To Do List", tasks=tasks)
+    return render_template('todos.html', title="To Do List", tasks=tasks, completed_tasks=completed_tasks)
 
 @app.route('/delete-task', methods=['POST'])
 def delete_task():
 
     task_id = int(request.form['task-id'])
     task = Task.query.get(task_id)
-    db.session.delete(task)
+    task.completed = True
+    db.session.add(task)
     db.session.commit()
 
     return redirect('/')
