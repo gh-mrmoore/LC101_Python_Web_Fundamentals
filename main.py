7bcce2e5b5d31288b63217fd773d6abb26bfdb0b
Matthew Moore <matt.rmoore@gmail.com>
2018-11-06 10:17:40 -0600

    Finished lessons 5-7

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -13,17 +13,35 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)   #every persistent class will need to have an id for database insertion
     name = db.Column(db.String(120))   #creating column 'name' with 120 characters maximum
     completed = db.Column(db.Boolean)
+    #owner_id = db.Column(db.Integer, db.ForeignKey('user_id'))
 
     def __init__(self, name):
         self.name = name
         self.completed = False
+        self.owner = owner
 
-@app.route('/', methods=['POST', 'GET'])
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    useremail = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+
+    def __init__(self, useremail, password):
+        self.useremail = useremail
+        self.password = password
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register']
+
+    if request.endpoint not in allowed_routes and 'useremail' not in session:
+        return redirect('/login')
+
+@app.route('/', methods=['POST', 'GET', 'PUT'])   #PUT method is used for updates from form data
 def index():
 
     if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name)
+        task_name = request.form['task']   #request.form.get will return "None" and allow better handling of any error... I need to try using this again...
+        new_task = Task(task_name)   #instantiation of the substantiated class, the parameters of the class must be passed thru in order to be valid
         db.session.add(new_task)
         db.session.commit()
     
@@ -32,7 +50,44 @@ def index():
 
     return render_template('todos.html', title="To Do List", tasks=tasks, completed_tasks=completed_tasks)
 
-@app.route('/delete-task', methods=['POST'])
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(useremail=email).first()
+
+        if user and user.password == password:
+            session['useremail'] = email
+            return redirect('/')
+        else:
+            return '<p>Error!</p>'
+    
+    return render_template('login.html')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        email = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify_password']
+
+        existing_user = User.query.filter_by(useremail=email).first()
+
+        if not existing_user:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['useremail'] = email
+            
+            return redirect ('/')
+        else:
+            return '<p>Duplicate User</p>'
+
+    return render_template('register.html')
+
+@app.route('/delete-task', methods=['POST'])   #could use a delete method in the form and add methods=['DELETE'] here in the code
 def delete_task():
 
     task_id = int(request.form['task-id'])
@@ -40,6 +95,12 @@ def delete_task():
     task.completed = True
     db.session.add(task)
     db.session.commit()
+
+    return redirect('/')
+
+@app.route('/logout')
+def logout():
+    del session['useremail']
 
     return redirect('/')
 
